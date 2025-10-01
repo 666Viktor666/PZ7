a0c6599a5b85753a9c5e058402ba955923593830
QA/auto <steputenkoviktor@gmail.com>
2025-10-01 12:56:25 +0300

    Add OOP classes: Employee hierarchy and Shape with inheritance

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Тестируем сотрудников
+        Employee emp1 = new Manager("Анна", 5000);
+        Employee emp2 = new Developer("Иван", 3000);
+
+        emp1.work();  // Анна управляет командой
+        emp2.work();  // Иван пишет код
+
+        // Тестируем фигуры
+        Shape shape1 = new Circle();
+        Shape shape2 = new Rectangle();
+
+        shape1.draw();  // Рисуется круг
+        shape2.draw();  // Рисуется прямоугольник
+    }
+}
